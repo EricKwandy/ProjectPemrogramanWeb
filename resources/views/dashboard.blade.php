eee45749bd5015c2540f7aaa66cbe04733bf51e4
Shouta002 <reynaldilight16@gmail.com>
2024-06-29 23:23:42 +0800

    id fixed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="py-2">
       <div class="flex-col">
-        <div class="bg-blue-800 max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           
 <div class="flex items-center justify-center p-12">
 
@@ -39,16 +39,16 @@
       </div>
       <div class="mb-5">
         <label
-          for="email"
+          for="studentId"
           class="mb-3 block text-base font-medium text-[#07074D]"
         >
           Subject
         </label>
         <input
           type="text"
-          name="email"
-          id="email"
-          placeholder="Enter your email"
+          name="studentId"
+          id="studentId"
+          placeholder="Enter your student id"
           class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         />
       </div>
@@ -62,7 +62,7 @@
         <textarea
           rows="2"
           name="address"
-          id="adress"
+          id="address"
           placeholder="Type your address"
           class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         ></textarea>
@@ -74,7 +74,7 @@
                                 <input type="file" name="image" id="image"
                                     class="w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-2 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
                             </div>
-      <div class="py-10">
+      <div class="pt-2">
         <button
           class="bg-[#3FA2F6] hover:bg-red-800 rounded-md py-3 px-8 text-base font-semibold text-black focus:outline-none"
         >
