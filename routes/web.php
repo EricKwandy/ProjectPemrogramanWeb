90ab7166730f9ee64c9206e5a761fe08df411bd1
Eric <erickwandy@gmail.com>
2024-06-30 22:49:02 +0800

    Test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DataController;
+use App\Http\Controllers\InputController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/store', [InputController::class, 'store'])->name('input_data');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
