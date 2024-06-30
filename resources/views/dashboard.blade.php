f32e11086cfa09c440c13f799df4e90a99b70bf3
Shouta002 <reynaldilight16@gmail.com>
2024-06-30 22:57:16 +0800

    updated form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,90 +1,91 @@
 <x-app-layout>
     <div class="py-2">
       <div class="flex-col">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          
-<div class="flex items-center justify-center p-12">
+        <div class="max-w-7xl mx-auto sm:px-4 lg:px-8">
+    
+          <div class="flex items-center justify-center sm:px-6 sm:p-6 p-12">
 
-  <div class="mx-auto w-full max-w-[550px]">
-    <form action="" method="POST">
-      <div class="mb-5">
-        <label
-          for="name"
-          class="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Full Name
-        </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Full Name"
-          class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      <div class="mb-5">
-        <label
-          for="email"
-          class="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Email Address
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="example@domain.com"
-          class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      <div class="mb-5">
-        <label
-          for="studentId"
-          class="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Subject
-        </label>
-        <input
-          type="text"
-          name="studentId"
-          id="studentId"
-          placeholder="Enter your student id"
-          class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      <div class="mb-5">
-        <label
-          for="address"
-          class="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Address
-        </label>
-        <textarea
-          rows="2"
-          name="address"
-          id="address"
-          placeholder="Type your address"
-          class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        ></textarea>
-      </div>
-      <div class="mb-5">
-                                <label for="image" class="mb-3 block text-base font-medium text-[#07074D]">
-                                    Upload Image
-                                </label>
-                                <input type="file" name="image" id="image"
-                                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-2 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                            </div>
-      <div class="pt-2">
-        <button
-          class="bg-[#3FA2F6] hover:bg-red-800 rounded-md py-3 px-8 text-base font-semibold text-black focus:outline-none"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
-  </div>
-</div>
+            <div class="mx-auto w-full max-w-[550px]">
+              <form action="" method="POST">
+                <div class="mb-5">
+                  <label
+                    for="name"
+                    class="mb-3 block text-base font-medium text-[#07074D]"
+                    >
+                    Full Name
+                  </label>
+                  <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Full Name"
+                  class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  />
+                </div>
+                <div class="mb-5">
+                  <label
+                    for="email"
+                    class="mb-3 block text-base font-medium text-[#07074D]"
+                    >
+                    Email Address
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="example@domain.com"
+                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  />
+                </div>
+                <div class="mb-5">
+                  <label
+                    for="studentId"
+                    class="mb-3 block text-base font-medium text-[#07074D]"
+                    >
+                    Student ID
+                  </label>
+                  <input
+                    type="text"
+                    name="studentId"
+                    id="studentId"
+                    placeholder="Enter your student id"
+                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  />
+                </div>
+                <div class="mb-5">
+                  <label
+                    for="address"
+                    class="mb-3 block text-base font-medium text-[#07074D]"
+                    >
+                    Address
+                  </label>
+                    <textarea
+                      rows="2"
+                      name="address"
+                      id="address"
+                      placeholder="Type your address"
+                      class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                      >
+                    </textarea>
+                </div>
+                <div class="mb-5">
+                  <label for="image" class="mb-3 block text-base font-medium text-[#07074D]">
+                    Upload Image
+                  </label>
+                  <input type="file" name="image" id="image"
+                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-2 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                </div>
+                <div class="pt-2">
+                  <button
+                    class="bg-[#2d9afa] hover:bg-blue-600  rounded-md sm:py-3 sm:px-8 px-6 py-2 text-base font-semibold text-black focus:outline-none"
+                    >
+                      Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </div>
 </x-app-layout>
